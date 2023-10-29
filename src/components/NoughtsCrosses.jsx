7a8495849a5b954157d9d19518ed2c7f5f9da19e
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-10-29 18:44:15 +0000

    update

--- src/components/NoughtsCrosses.jsx
+++ src/components/NoughtsCrosses.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import Board from "./Board";
-import GameOver from "./GameOver";
-import Reset from "./Reset";
+//import GameOver from "./GameOver";
+//import Reset from "./Reset";
 import { useState } from 'react';
 
 const UserPlayer_X = 'X';
 const UserPlayer_O = 'O';
 
-const handleClickTile = (index) => {
-    console.log(index)
-}
 
 
 
 const NoughtsCrosses = () => {
     const [tiles, setTiles] = useState(Array(9).fill(null))
     const [userTurn, setuserTurn] = useState(UserPlayer_X)
+
+    const handleClickTile = (index) => {
+//if statement that checkes the value
+if(tiles[index] !== null) {
+    return;
+}
+        console.log(index)
+        const newTiles = [...tiles];
+        newTiles[index] = userTurn
+        setTiles(newTiles)
+        if(userTurn === UserPlayer_X){
+            setuserTurn (UserPlayer_O);
+        } else {
+            setuserTurn(UserPlayer_X)
+        }
+    }
+    
     return (
         <div>
          <h1>Noughts & Crosses</h1>
          <Board tiles={tiles} onClickTile={handleClickTile} />
-         <GameOver />
-         <Reset />
+    
         </div>
     );
 }
